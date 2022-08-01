a5fcbc2d25968ee581f03132f3a9e253de917d08
NoaLeGeek <62206118+NoaLeGeek@users.noreply.github.com>
2022-08-01 14:50:30 +0200

    Update CommandInfo.java

--- src/main/java/fr/sunderia/sunderiautils/commands/CommandInfo.java
+++ src/main/java/fr/sunderia/sunderiautils/commands/CommandInfo.java
@@ -42,5 +42,10 @@ public @interface CommandInfo {
     /**
      * @return The message sent to the player if he doesn't have the permission to execute the command
      */
-    String permissionMessage() default "§cYou don't have permission to use this command.";
+    String permissionMessage() default "You don't have permission to use this command.";
+    
+    /**
+     * @return The color that will be used for permissionMessage
+     */
+    ChatColor permissionMessageColor() default ChatColor.RED
 }
