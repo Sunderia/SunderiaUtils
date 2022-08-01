50072f80aa0867d2e32974ee4fad5fcab0e91f91
NoaLeGeek <62206118+NoaLeGeek@users.noreply.github.com>
2022-08-01 14:51:01 +0200

    Update PluginCommand.java

--- src/main/java/fr/sunderia/sunderiautils/commands/PluginCommand.java
+++ src/main/java/fr/sunderia/sunderiautils/commands/PluginCommand.java
@@ -43,7 +43,7 @@ public abstract class PluginCommand extends BukkitCommand {
     @Override
     public boolean execute(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] args) {
         if(!info.permission().isEmpty() && !sender.hasPermission(info.permission())) {
-            sender.sendMessage(ChatColor.RED + info.permissionMessage());
+            sender.sendMessage(info.permissionMessageColor() + info.permissionMessage());
             return true;
         }
         if(info.requiresPlayer()) {
